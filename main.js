13955008f926f48ffafb5626b663e1902c55c365
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-06-07 12:00:07 -0400

    adding test fetch

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 addEventListener("click", (event) => {})
+
+
+const fetchData = () => {
+
+    fetch("https://api.artic.edu/api/v1/artworks")
+    .then(response => response.json())
+    .then(data => renderImages(data.data))
+}
