3667afa630c7e6be3fd0d44305596723c025bfe1
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-07-19 10:47:20 -0400

    testing js and css

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 addEventListener("click", (event) => {})
-
+let x = "string"
 
 const fetchData = () => {
 
@@ -8,38 +8,4 @@ const fetchData = () => {
     .then(data => renderImages(data.data))
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //const currentTime = () => {
-//     const el = document.querySelector("h1")
-
-//     let date = new Date()
-//     let gg = date.getHours()
-//     let mm = date.getMinutes()
-//     let ss = date.getSeconds()
-
-//     hh =hh < 10 ? `0${hh}` : hh
-//     mm =mm < 10 ? `0${mm}` : mm
-//     ss =ss < 10 ? `0${ss}` : ss
-
-//     let time = `${hh}:${mm}:${ss}`
-//     el.innterText = time
-
-// }
-
-// currentTime()
-// setInterval(currentTime, 1000)//
+console.log(x)
