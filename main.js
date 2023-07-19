3d395200a4dd1165e2a273c73bda917256ced95c
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-07-19 10:31:35 -0400

    commented out time test code

--- main.js
+++ main.js
@@ -9,22 +9,37 @@ const fetchData = () => {
 }
 
 
-const currentTime = () => {
-    const el = document.querySelector("h1")
 
-    let date = new Date()
-    let gg = date.getHours()
-    let mm = date.getMinutes()
-    let ss = date.getSeconds()
 
-    hh =hh < 10 ? `0${hh}` : hh
-    mm =mm < 10 ? `0${mm}` : mm
-    ss =ss < 10 ? `0${ss}` : ss
 
-    let time = `${hh}:${mm}:${ss}`
-    el.innterText = time
 
-}
 
-currentTime()
-setInterval(currentTime, 1000)
+
+
+
+
+
+
+
+
+
+
+// //const currentTime = () => {
+//     const el = document.querySelector("h1")
+
+//     let date = new Date()
+//     let gg = date.getHours()
+//     let mm = date.getMinutes()
+//     let ss = date.getSeconds()
+
+//     hh =hh < 10 ? `0${hh}` : hh
+//     mm =mm < 10 ? `0${mm}` : mm
+//     ss =ss < 10 ? `0${ss}` : ss
+
+//     let time = `${hh}:${mm}:${ss}`
+//     el.innterText = time
+
+// }
+
+// currentTime()
+// setInterval(currentTime, 1000)//
