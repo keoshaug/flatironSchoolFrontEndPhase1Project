01e268a4a5eaa4af20e60e8e862a765091452b74
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-07-19 11:56:25 -0400

    adding the function getArtworks and making sure it retrieves data

--- main.js
+++ main.js
@@ -5,9 +5,15 @@ const fetchData = () => {
 
     fetch("https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&hasImages=true&q=impressionism")
     .then(response => response.json())
-    .then(data => console.log(data.objectIDs))
+    .then(data => getArtworks(data.objectIDs))
+
+    function getArtworks(artData) {
+        console.log(artData)
+   }
+   
 }
 
 fetchData()
+
 
 
