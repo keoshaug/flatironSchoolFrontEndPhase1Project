f592cbf2e467b76ef1b8e64a55d795358ccaea04
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-08-14 12:20:03 -0400

    adding JS for back to top button

--- main.js
+++ main.js
@@ -14,6 +14,27 @@ addEventListener("search", (e) => {
     console.log(e.target.scrollTop)
  })
 
+ const showOnPx = 100;
+const backToTopButton = document.querySelector(".back-to-top")
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+document.addEventListener("scroll", () => {
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden")
+  } else {
+    backToTopButton.classList.add("hidden")
+  }
+})
+
+ const goToTop = () => {
+  document.body.scrollIntoView();
+};
+
+backToTopButton.addEventListener("click", goToTop)
+
 
 
 const API = "https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&hasImages=true&q=impressionism"
