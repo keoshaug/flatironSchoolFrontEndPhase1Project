4a3fa070f6a53461949122fcd9241cd7d9e2fb1f
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-08-15 16:19:53 -0400

    restructuring fetches to better render api data

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+
+
 const displayData = document.querySelector("#display-data")
+const searchbar = document.getElementById("searchbar")
 
 
 addEventListener("submit", (e) => {
@@ -30,53 +33,37 @@ const API = "https://collectionapi.metmuseum.org/public/collection/v1/search?isH
 const cardContainer = document.getElementById("card-container")
 
 
-const fetchData = () => {
-
-    fetch(API)
-    .then(response => response.json())
-    .then(data => getArtworks(data.objectIDs))
-    
-
-
-    function getArtworks(artData) {
-        artData.forEach((objectID) => {
-            const img = document.createElement("img")
-
-              if(img.style.height >= img.style.width) {
-                img.style.height = '300px'
-              } else {
-                img.style.width = '300px'
-              }
-            fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectID}`)
-            .then(response => response.json())
-            .then(data => (img.src = data.primaryImage))
-
-            const card = document.createElement("card")
-            card.classList.add("card")
-
-            
-            const department = document.createElement("department")
-            department.textContent = objectID.department
-    
-            const title = document.createElement("title")
-            title.textContent = objectID.title
-
-            const artistDisplayName = document.createElement("artistDisplayName")
-            artistDisplayName.textContent = objectID.artistDisplayName
-
-            const objectDate = document.createElement("objectDate")
-            objectDate.textContent = objectID.objectDate
-
-          
-
-            card.append(img, department, title, artistDisplayName, objectDate)
-
-            cardContainer.appendChild(img)
-            
-        })     
-   }
-   
+const getArtworks = (dataArray) => {
+  console.log(dataArray)
 }
 
-fetchData()
+
+
+fetch(API)
+.then(response => response.json())
+.then(data => getArtworks(data.objectIDs))
+
+    
+
+  //   function getArtworks(artData) {
+  //       artData.forEach((objectID) => {
+  //           const img = document.createElement("img")
+
+  //             if(img.style.height >= img.style.width) {
+  //               img.style.height = '300px'
+  //             } else {
+  //               img.style.width = '300px'
+  //             }
+  //           fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectID}`)
+  //           .then(response => response.json())
+  //           .then(data => (img.src = data.primaryImage))
+            
+
+            
+  //       })     
+  //  }
+   
+
+
+// fetchData()
 
