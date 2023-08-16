a62172e710a0417673ac0eb1edad67666464617d
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-08-16 13:43:50 -0400

    changing code to restore images on screen

--- main.js
+++ main.js
@@ -27,47 +27,58 @@ backToTopButton.addEventListener("click", function() {
 backToTopButton.addEventListener("click", returnToTop)
 
 
-
-
 const API = "https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&hasImages=true&q=impressionism"
 const cardContainer = document.getElementById("card-container")
 
 
 
-
-
 function fetchData () {
+
+
+  fetch(API)
+  .then(response => response.json())
+  .then(data => getArtworks(data.objectIDs))
+  
+
+
+  function getArtworks(artData) {
+      artData.forEach((objectID) => {
+          const img = document.createElement("img")
+
+            if(img.style.height >= img.style.width) {
+              img.style.height = '300px'
+            } else {
+              img.style.width = '300px'
+            }
+          fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectID}`)
+          .then(response => response.json())
+          .then(data => (img.src = data.primaryImage))
+
+          const card = document.createElement("card")
+          card.classList.add("card")
+
+          
+          const department = document.createElement("department")
+          department.textContent = objectID.department
+  
+          const title = document.createElement("title")
+          title.textContent = objectID.title
+
+          const artistDisplayName = document.createElement("artistDisplayName")
+          artistDisplayName.textContent = objectID.artistDisplayName
+
+          const objectDate = document.createElement("objectDate")
+          objectDate.textContent = objectID.objectDate
+
+        
+
+          card.append(img, department, title, artistDisplayName, objectDate)
+
+          cardContainer.appendChild(img)
+          
+      })     
+ }
  
-
-fetch(API)
-.then(response => response.json())
-.then(data => getArtworks(data.objectIDs))
-
-const getArtworks = (dataArray) => {
-  dataArray.forEach(objectID => {
-    console.log(dataArray)
-  })
 }
-  const img = document.createElement("img")
-  const objectID = function getArtwork(artData) {
-  artData.forEach((objectID) => {
-
-  fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectID}`)
-            .then(response => response.json())
-            .then(data => data(img.src = data.primaryImage))
-
-              if(img.style.height >= img.style.width) {
-                img.style.height = '300px'
-              } else {
-                img.style.width = '300px'
-              }
-            
-
-            
-        })     
-   }
-}
-
 
 fetchData()
-
